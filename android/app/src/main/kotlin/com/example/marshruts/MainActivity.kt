8b8632905a3e2092f259ab3a8642305801ee2f3e
sadykovs <sadykovs@gmail.com>
2022-10-18 13:20:08 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/marshruts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.marshruts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Routing and Navigation'),
+          centerTitle: true,
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Move to Page 2'),
+      ),
+    );
+  }
+}
